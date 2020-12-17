e5bb34a969fc7acf7ad719e1f7a6d5d7577dbe8e
Ali Akhtari <aaliiaakhtarii@gmail.com>
2020-12-17 21:46:50 +0330

    [feat] Define strategy

---
+++ src/strategy.js
@@ -0,0 +1,21 @@
+const { SYMBOLS, TIME_FRAMES } = require("bfx-hf-util");
+const { BollingerBands } = require("bfx-hf-indicators");
+const HFS = require("bfx-hf-strategy");
+
+module.exports = ({
+  symbol = SYMBOLS.BTC_USD,
+  tf = TIME_FRAMES.ONE_DAY,
+} = {}) =>
+  HFS.define({
+    id: "bbands",
+    name: "bbands",
+    symbol,
+    tf,
+
+    indicators: {
+      bbands2: new BollingerBands([20, 2]),
+      bbands1: new BollingerBands([20, 1]),
+    },
+
+    onEnter: on_enter,
+  });
